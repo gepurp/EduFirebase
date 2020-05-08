1f3ee5ff44d3e7e3b629f7f1db0f8d96ac07b82d
octopus <generpurpo@gmail.com>
2020-05-08 10:11:21 +0300

    Connected to the Firebase. Added Firebase Authentication.

--- app/src/main/java/com/edu/edufirebase/MainActivity.java
+++ app/src/main/java/com/edu/edufirebase/MainActivity.java
@@ -3,12 +3,28 @@ package com.edu.edufirebase;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText edtEmail;
+    private EditText edtUsername;
+    private EditText edtPassword;
+    private Button btnSingUp;
+    private Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        edtEmail = findViewById(R.id.edtEmail);
+        edtUsername = findViewById(R.id.edtUsername);
+        edtPassword = findViewById(R.id.edtPassword);
+        btnSingUp = findViewById(R.id.btnSingUp);
+        btnLogin = findViewById(R.id.btnLogin);
+
+
     }
 }
