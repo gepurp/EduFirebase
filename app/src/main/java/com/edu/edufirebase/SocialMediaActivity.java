05d9a07a3ac77301a0320f9ac3626606f1891f87
octopus <generpurpo@gmail.com>
2020-05-09 08:30:12 +0300

    Added menu for sign out functionality

--- app/src/main/java/com/edu/edufirebase/SocialMediaActivity.java
+++ app/src/main/java/com/edu/edufirebase/SocialMediaActivity.java
@@ -1,19 +1,57 @@
 package com.edu.edufirebase;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SocialMediaActivity extends AppCompatActivity {
 
-    private TextView txtViewWelcome;
+    // Firebase Auth declaration
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_social_media);
 
+        // Firebase Auth initialization
+        mAuth = FirebaseAuth.getInstance();
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.app_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+
+            case R.id.logoutItem:
+
+                signOut();
+
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        signOut();
+    }
+
+    private void signOut() {
+        mAuth.signOut();
+        finish();
     }
 }
