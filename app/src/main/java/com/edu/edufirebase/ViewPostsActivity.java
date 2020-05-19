2043ab0b1a65f1373b51d8408c62c049b2017527
octopus <generpurpo@gmail.com>
2020-05-19 20:15:03 +0300

    Implemented functionality for viewing posts

--- app/src/main/java/com/edu/edufirebase/ViewPostsActivity.java
+++ app/src/main/java/com/edu/edufirebase/ViewPostsActivity.java
@@ -1,14 +1,76 @@
     package com.edu.edufirebase;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class ViewPostsActivity extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+
+    public class ViewPostsActivity extends AppCompatActivity {
+
+    private ListView postsListView;
+    private ArrayList<String> userNames;
+    private ArrayAdapter arrayAdapter;
+    private FirebaseAuth firebaseAuth;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_posts);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        postsListView = findViewById(R.id.postsListView);
+
+        userNames = new ArrayList<>();
+        arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, userNames);
+        postsListView.setAdapter(arrayAdapter);
+
+        FirebaseDatabase.getInstance().getReference()
+                .child("users")
+                .child(firebaseAuth.getCurrentUser().getUid())
+                .child("received_posts")
+                .addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+                String fromWhom = (String) dataSnapshot.child("from_whom").getValue();
+                userNames.add(fromWhom);
+
+                arrayAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 }
